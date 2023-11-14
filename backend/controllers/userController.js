d6deb0da3c37ed2fade72a91262fd374be5c9d8a
shehab124 <chehab.gamal@gmail.com>
2023-11-14 21:20:30 +0200

    added SignUp, hashind, salting

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,12 +3,21 @@ const userModel = require('../models/userModel');
 
 // login user
 const loginUser = async (req, res) => {
+
     res.json({ mssg: 'login user' })
 }
 
 // signup user
 const signupUser = async (req, res) => {
-    res.json({ mssg: 'signup user' })
+    const { email, password } = req.body;
+    try {
+        const user = await userModel.signup(email, password);
+        res.status(200).json({ user })
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message })
+        console.log(error);
+    }
 }
 
 module.exports = { loginUser, signupUser };
