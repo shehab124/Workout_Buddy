a078fd3227fc914c5f449511ea47f7659267974c
shehab124 <chehab.gamal@gmail.com>
2023-11-16 02:19:50 +0200

    added useLogin hook

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 
 const useLogout = () => {
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,32 +1,19 @@
 import React, { useState } from 'react';
+import useLogin from '../hooks/useLogin';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [emptyFields, setEmptyFields] = useState([]);
-
+    const { login, error, isLoading } = useLogin();
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        const user = { email, password };
-
-        const response = fetch('/api/user/login', {
-            method: 'POST',
-            body: JSON.stringify(user),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-
-        const json = await response.json();
-
-        if (!response.ok) {
-            setError(json.error)
-            setEmptyFields(json.emptyFields);
-        }
+        await login(email, password);
+        setEmail('');
+        setPassword('');
     }
+
 
     return (
         <form className='login' onSubmit={handleSubmit}>
@@ -43,7 +30,8 @@ const Login = () => {
                 onChange={(e) => { setPassword(e.target.value) }}
                 value={password}
             />
-            <button>Log In</button>
+            <button disabled={isLoading}>Log In</button>
+            {error && <div className='error'>{error}</div>}
         </form>
     );
 }
