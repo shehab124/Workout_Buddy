2670ad14491bafb67bd2217484e5fe2dff6b0630
shehab124 <chehab.gamal@gmail.com>
2023-11-18 13:39:01 +0200

    Redirection using Navigate component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext';
 
 // pages & components
 import Home from './pages/Home';
@@ -7,15 +8,17 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 
 function App() {
+  const { user } = useAuthContext()
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/login' element={<Login />} />
+            <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+            <Route path='/signup' element={!user ? <Signup /> : <Navigate to="/" />} />
+            <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
           </Routes>
         </div>
       </BrowserRouter>
